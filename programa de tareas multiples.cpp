441266a3141e68e26504b4984279a224e99d76c1
josevargas0310 <jvargash1@miumg.edu.gt>
2022-03-06 00:34:34 -0600

    se le agrego al programa la funcion de sacar el factorial de un numero, para esto se creo la funcion, funcionfactorial, y se le declaro el proceso que tiene que realizar

--- programa de tareas multiples.cpp
+++ programa de tareas multiples.cpp
@@ -1,21 +1,25 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 
 //funciones del codigo 
 void menu(); 
+int funcionfactorial(int num);
 
 //declaracion de las variables 
 int opcion; //variable del menu 
+int a; //varible donde se guarda el numero ingresado del apartado de factorial 
+int num;
 
 //llamado de las funciones 
 int main()
 {
 	menu();
 	
-	return 0; 
+ return 0;	
 }
 
 void menu(){ 
-	printf("a continuacion digite el numero correspondiente a la funcion  que desea que se opere operar.");
+	printf("A continuacion digite el numero correspondiente a la funcion  que desea que se opere.");
 	printf("\n 1. calcular el factorial de un numero.  ");
 	printf("\n 2.Tabla de multiplicar de un numero  ");
 	printf("\n 3.calculo de 10 numeros pares e impares");
@@ -24,6 +28,29 @@ void menu(){
 	
 	printf("\n Digite la opcion deseada");
 	scanf ("%i" , &opcion);
+
+
+ switch (opcion)
+{
+	case 1: cout<<"Ingrese el numero que desea operar: "<<endl;
+	cin>>a;
+	cout<<"El factorial de "<<a<<" es "<<funcionfactorial(a)<<endl;
+	
+	break;
 	
 	
-}
+	
+	
+}	
+ } 
+//se llama la funcion y se le declara el proceso que hara 
+int funcionfactorial(int num)
+	{
+	int fact=1; //variable para una comparacion en while del aprtado de factoriales
+	while(num>0)
+	   {
+	   	fact=fact*num--; 
+		   }	
+		   return fact;
+	   
+	}
